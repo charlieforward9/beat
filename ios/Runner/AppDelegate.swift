ed5b98af4794fe9d9e8b7e212f61955e3e805fb1
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2022-07-05 19:59:31 -0400

    Removed Google Maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,8 +8,6 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-
-    GMSServices.provideAPIKey("AIzaSyAu4KxiJ5kliSGYenTSpC19eG5o3OeIOFI")
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/Common_Widgets/edit_time_budget_widget.dart
+++ lib/Common_Widgets/edit_time_budget_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class EditTimeBudget extends StatefulWidget {
   EditTimeBudget({Key? key}) : super(key: key);
@@ -9,28 +8,14 @@ class EditTimeBudget extends StatefulWidget {
 }
 
 class _EditTimeBudgetState extends State<EditTimeBudget> {
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(29.643633, -82.354927);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Maps Sample App'),
-              backgroundColor: Colors.green[700],
-            ),
-            body: GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 11.0,
-              ),
-            )));
+      appBar: AppBar(
+        title: const Text('Maps Sample App'),
+        backgroundColor: Colors.green[700],
+      ),
+    ));
   }
 }
