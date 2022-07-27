6c0ea1dde09841082b20658bd2b5ddead733eaee
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2022-07-27 13:55:48 -0400

    Fixing colors merge

--- lib/views/time_budget_widgets/time_budget_widget.dart
+++ lib/views/time_budget_widgets/time_budget_widget.dart
@@ -1,44 +1,18 @@
 import 'package:flutter/material.dart';
 
-//*************** Services Import *************** */
-// import '../../Features/Fitness/services/FitnessService.dart';
-// import '../../Features/Recovery/services/RecoveryService.dart';
-// import '../../Features/Fuel/services/FuelService.dart';
-// import '../../Features/Network/services/NetworkService.dart';
-// import '../../Features/Productivity/services/ProductivityService.dart';
-
-// sleep
-import 'dart:io';
-
 class GoalCard extends StatefulWidget {
   final String cardName;
   final String cardGoal;
   final Color passedColor;
-  final String ID;
 
   final TextEditingController updatedGoal = TextEditingController();
   final TextEditingController updatedPercentage = TextEditingController();
-
-  //*************** Services *************** */
-  // FitnessService fitnessService = FitnessService();
-  // RecoveryService recoveryService = RecoveryService();
-  // FuelService fuelService = FuelService();
-  // NetworkService networkService = NetworkService();
-  // ProductivityService productivityService = ProductivityService();
-  //*************** ID FOR TESTING *************** */
-  final fitnessID = "c6a235e6-a42f-49f7-b9d2-0656c91d0880";
-  final recoveryID = "c460788a-7519-49f6-baa0-81624b4d0748";
-  final fuelID = "b403bc49-60dd-4b82-aa9c-2c2c6ac99765";
-  final networkID = "57863e60-1f20-4bf9-89a5-c05968475a29";
-  final productivityID = "ffbdee44-436e-4560-8d30-562235986c85";
-  //*********************************************** */
 
   GoalCard(
       {Key? key,
       required this.cardName,
       required this.cardGoal,
-      required this.passedColor,
-      required this.ID})
+      required this.passedColor})
       : super(key: key);
 
   @override
@@ -70,6 +44,10 @@ class _GoalCardState extends State<GoalCard> {
                 //Spacer(),
                 TextButton.icon(
                   onPressed: () {
+                    //TODO Where is this widget?
+                    //Navigator.of(context).push(MaterialPageRoute(
+                    //   builder: (context) => EditTimeBudget()));
+
                     // // ========= EXAMPLE ===========
                     // if (widget.cardName == "Recovery") {
                     //   widget.recoveryService
@@ -176,66 +154,67 @@ class _GoalCardState extends State<GoalCard> {
 
   void _updatedSpecificGoal(_goal, _progress) async {
     if (widget.cardName == "Recovery") {
-    //   await widget.recoveryService.resetGoal(widget.recoveryID, _goal);
-    //   widget.recoveryService.updateProgress(widget.recoveryID, _progress);
-    // } else if (widget.cardName == "Fitness") {
-    //   await widget.fitnessService.resetGoal(widget.fitnessID, _goal);
-    //   widget.fitnessService.updateProgress(widget.fitnessID, _progress);
-    // } else if (widget.cardName == "Network") {
-    //   await widget.networkService.resetGoal(widget.networkID, _goal);
-    //   widget.networkService.updateProgress(widget.networkID, _progress);
-    // } else if (widget.cardName == "Fuel") {
-    //   await widget.fuelService.resetGoal(widget.fuelID, _goal);
-    //   widget.fuelService.updateProgress(widget.fuelID, _progress);
-    // } else if (widget.cardName == "Productivity") {
-    //   await widget.productivityService.resetGoal(widget.productivityID, _goal);
-    //   widget.productivityService
-    //       .updateProgress(widget.productivityID, _progress);
-    // }
+      //   await widget.recoveryService.resetGoal(widget.recoveryID, _goal);
+      //   widget.recoveryService.updateProgress(widget.recoveryID, _progress);
+      // } else if (widget.cardName == "Fitness") {
+      //   await widget.fitnessService.resetGoal(widget.fitnessID, _goal);
+      //   widget.fitnessService.updateProgress(widget.fitnessID, _progress);
+      // } else if (widget.cardName == "Network") {
+      //   await widget.networkService.resetGoal(widget.networkID, _goal);
+      //   widget.networkService.updateProgress(widget.networkID, _progress);
+      // } else if (widget.cardName == "Fuel") {
+      //   await widget.fuelService.resetGoal(widget.fuelID, _goal);
+      //   widget.fuelService.updateProgress(widget.fuelID, _progress);
+      // } else if (widget.cardName == "Productivity") {
+      //   await widget.productivityService.resetGoal(widget.productivityID, _goal);
+      //   widget.productivityService
+      //       .updateProgress(widget.productivityID, _progress);
+      // }
+    }
   }
 }
 
-// class ButtonRow extends StatefulWidget {
-//   final buttonOneName;
-//   final buttonTwoName;
-//   const ButtonRow({
-//     Key? key,
-//     required this.buttonOneName,
-//     required this.buttonTwoName,
-//   }) : super(key: key);
+class ButtonRow extends StatefulWidget {
+  final buttonOneName;
+  final buttonTwoName;
+  const ButtonRow({
+    Key? key,
+    required this.buttonOneName,
+    required this.buttonTwoName,
+  }) : super(key: key);
 
-//   @override
-//   State<ButtonRow> createState() => _ButtonRowState();
-// }
+  @override
+  State<ButtonRow> createState() => _ButtonRowState();
+}
 
-// class _ButtonRowState extends State<ButtonRow> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//       children: [
-//         SizedBox(width: 25),
-//         Expanded(
-//             child: ElevatedButton(
-//                 onPressed: () {
-//                   // Respond to button press
-//                 },
-//                 child: Text('${widget.buttonOneName}'),
-//                 style: ElevatedButton.styleFrom(
-//                   primary: Colors.deepOrangeAccent,
-//                 ))),
-//         SizedBox(width: 25),
-//         Expanded(
-//             child: ElevatedButton(
-//                 onPressed: () {
-//                   // Respond to button press
-//                 },
-//                 child: Text('${widget.buttonTwoName}'),
-//                 style: ElevatedButton.styleFrom(
-//                   primary: Colors.deepOrangeAccent,
-//                 ))),
-//         SizedBox(width: 25),
-//       ],
-//     );
-//   }
+class _ButtonRowState extends State<ButtonRow> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SizedBox(width: 25),
+        Expanded(
+            child: ElevatedButton(
+                onPressed: () {
+                  // Respond to button press
+                },
+                child: Text('${widget.buttonOneName}'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.deepOrangeAccent,
+                ))),
+        SizedBox(width: 25),
+        Expanded(
+            child: ElevatedButton(
+                onPressed: () {
+                  // Respond to button press
+                },
+                child: Text('${widget.buttonTwoName}'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.deepOrangeAccent,
+                ))),
+        SizedBox(width: 25),
+      ],
+    );
+  }
 }
