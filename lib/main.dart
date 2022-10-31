cf76e25cab4edd3ef112b25938ab2e340a588424
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2022-10-30 21:07:22 -0400

    whack a mole config and some cubit fixes

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,11 @@
 //export PATH=/Users/crich/Documents/flutter/bin:$PATH
 
 //***********Backend-related Imports***********//
-import 'dart:developer';
+import 'package:beat/cubits/amplify_cubit.dart';
 import 'package:beat/cubits/auth_cubit.dart';
 import 'package:beat/config/locale_config.dart';
-import 'package:beat/config/amplify_config.dart';
-import 'package:amplify_flutter/amplify_flutter.dart';
 //***********Frontend-related Imports***********//
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:beat/views/init_view.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
@@ -18,96 +15,66 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final AmplifyConfiguration _amplifyConfig = AmplifyConfiguration();
-  bool _amplifyConfigured = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _amplifyConfig.init().whenComplete(
-      () {
-        if (kDebugMode) {
-          syncListener();
-          _amplifyConfig.configDev();
-        } else {
-          _amplifyConfig
-              .configProd()
-              .then((_) => setState(() => _amplifyConfigured = true));
-        }
-      },
-    );
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    if (_amplifyConfigured) {
-      return Authenticator(
-        signUpForm: _amplifyConfig.signUpForm,
-        child: MaterialApp(
-          localizationsDelegates: localizationsDelegates,
-          supportedLocales: supportedLocales,
-          builder: Authenticator.builder(),
-          title: 'BEAT',
-          home: Scaffold(
-            body: BlocProvider(
-              create: (context) => AuthCubit()..authUser(),
-              child: BlocBuilder<AuthCubit, AuthState>(
-                builder: (context, state) {
-                  if (state is AuthSuccess) {
-                    return const InitPage();
-                  } else if (state is AuthFail) {
-                    return Center(
-                        child:
-                            Text("Auth Failed with state ${state.exception}"));
-                  } else {
-                    //state is Unauth()
-                    return Center(
-                      child: Column(children: const [
-                        Text("Unauthorized"),
-                        CircularProgressIndicator()
-                      ]),
-                    );
-                  }
-                },
+    return BlocProvider<AmplifyCubit>(
+      create: (context) => AmplifyCubit()..init(),
+      child: BlocBuilder<AmplifyCubit, AmplifyState>(
+        builder: (context, state) {
+          if (state is AmplifyConfigured) {
+            return Authenticator(
+              signUpForm: BlocProvider.of<AmplifyCubit>(context).signUpForm,
+              child: MaterialApp(
+                localizationsDelegates: localizationsDelegates,
+                supportedLocales: supportedLocales,
+                builder: Authenticator.builder(),
+                title: 'BEAT',
+                home: Scaffold(
+                  body: BlocProvider(
+                    create: (context) => AuthCubit()..authUser(),
+                    child: BlocBuilder<AuthCubit, AuthState>(
+                      builder: (context, state) {
+                        if (state is AuthSuccess) {
+                          //TODO Determine if its a first time or returning user
+                          return const InitPage();
+                        } else if (state is AuthFail) {
+                          return Center(
+                              child: Text(
+                                  "Auth Failed with state ${state.exception}"));
+                        } else {
+                          //state is Unauth()
+                          return Center(
+                            child: Column(children: const [
+                              Text("Unauthorized"),
+                              CircularProgressIndicator()
+                            ]),
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ),
-      );
-    }
-    return const MaterialApp(
-        title: "Not Config",
-        home: Scaffold(
-          body: Center(child: Text("Amplify Not Configured")),
-        ));
-  }
-
-  //Delays queries until DataStore is completely synced
-  void syncListener() {
-    //Only do this if the user is logged in
-    Amplify.Auth.fetchAuthSession().then(((session) {
-      if (session.isSignedIn) {
-        Amplify.Hub.listen(([HubChannel.DataStore]), (hubEvent) {
-          log("${hubEvent.eventName} ${hubEvent.payload}");
-
-          if (hubEvent.eventName == 'ready') {
-            log("Datastore ready to query");
-            setState(() {
-              _amplifyConfigured = true;
-            });
+            );
+          } else if (state is AmplifyFailure) {
+            return MaterialApp(
+                title: "Not Config",
+                home: Scaffold(
+                    body: Center(
+                  child: Text(state.exception.toString()),
+                )));
+          } else {
+            //state is AmplifyNotConfigured
+            return const MaterialApp(
+                title: "Not Config",
+                home: Scaffold(
+                    body: Center(
+                  child: Text('Amplify not configured'),
+                )));
           }
-        });
-      } else {
-        setState(() {
-          _amplifyConfigured = true;
-        });
-      }
-    }));
+        },
+      ),
+    );
   }
 }
